f0b02c574707cbf78cc08647509c15b908f513db
CMLTTN <cemaldim@gmail.com>
2023-07-22 15:37:58 +0300

    form

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ProductForm from "../components/ProductForm"
 
 
 const NewProduct = () => {
+
+  const [formData, setFormData] = useState("")
+
+  const initialState = {
+  name:"",
+  image:"",
+  price:"",
+  dampingRate:0.2,
+  amount:1
+  
+  }
   
 
   return (
     <div className='container'>
+
+    <ProductForm/>
       
     </div>
   );
