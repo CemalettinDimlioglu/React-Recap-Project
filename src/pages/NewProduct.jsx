ca35daefb5e43a0d82c40e0f6bdf506069119925
CMLTTN <cemaldim@gmail.com>
2023-07-22 16:29:15 +0300

    axios

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,25 +1,47 @@
 import React, {useState} from 'react'
 import ProductForm from "../components/ProductForm"
+import axios from 'axios';
 
+const initialState = {
+  name: "",
+  image: "",
+  price: "",
+  dampingRate: 0.2,
+  amount: 1,
+};
+  
 
 const NewProduct = () => {
+   const url = "https://64b83b6221b9aa6eb079b2d2.mockapi.io/recap";
 
-  const [formData, setFormData] = useState("")
+  const [formData, setFormData] = useState(initialState)
 
-  const initialState = {
-  name:"",
-  image:"",
-  price:"",
-  dampingRate:0.2,
-  amount:1
+
+  const handleChane = (e) => {
+    setFormData({...formData, [e.target.id]:e.target.value})
+
+    console.log({ ...formData, [e.target.id]: e.target.value });
   
   }
+
+   const handleSubmit = async (e) => {
+     e.preventDefault();
+     try {
+       await axios.post(url, formData);
+       setFormData(initialState);
+     } catch (error) {}
+   };
+
+  console.log(initialState);
+
+
+
   
 
   return (
     <div className='container'>
 
-    <ProductForm/>
+    <ProductForm  handleChange={handleChane} formData={formData}  handleSubmit={handleSubmit}/>
       
     </div>
   );
